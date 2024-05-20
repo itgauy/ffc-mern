8345006231e849ec4f79017583bfd69d64641602
itgauy <torreslian@duck.com>
2024-05-20 11:54:50 +0800

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) { // Redirection or route
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/json", function (req, res) {
+  res.json({ "message": "Hello json" });
+});
+
 app.use("/", express.static(__dirname + '/public')); // To access the styles.css for index.html
 app.use("/public", express.static(__dirname + '/public'));
 
