180e6802ba68838baf5562db1ce6a20ce640647b
itgauy <torreslian@duck.com>
2024-05-20 11:48:13 +0800

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,8 +3,11 @@ let app = express();
 
 console.log('Hello');
 
-app.get("/", function (req, res) {
+app.get("/", function (req, res) { // Redirection or route
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use("/", express.static(__dirname + '/public')); // To access the styles.css for index.html
+app.use("/public", express.static(__dirname + '/public'));
 
 module.exports = app; 
