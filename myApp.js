80583e70be035b499a3a6f1b3457067adad8a1ea
itgauy <torreslian@duck.com>
2024-05-20 18:22:49 +0800

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 console.log('Hello');
 
 app.get("/", function (req, res) { // Redirection or route
